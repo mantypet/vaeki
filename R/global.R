fed0b298d39f8986b5faa6ced6e024248d981249
Petteri Mäntymaa <petteri.mantymaa@thl.fi>
2023-09-13 15:31:44 +0300

    reader

--- R/global.R
+++ R/global.R
@@ -1,4 +1,7 @@
 library(dplyr)
+library(data.table)
+library(tidyr)
+library(ggplot2)
 
 # Disclaimer: This page uses data from the OpenPowerlifting project, https://www.openpowerlifting.org.
 # You may download a copy of the data at https://data.openpowerlifting.org.
@@ -6,4 +9,19 @@ library(dplyr)
 download_opl <- function() {
   url <- "https://openpowerlifting.gitlab.io/opl-csv/files/openpowerlifting-latest.zip"
   download.file(url, here::here("data/"))
+}
+
+read_opl <- function(csv = here::here("data/openpowerlifting-2023-09-09/openpowerlifting-2023-09-09-6e6c522a.csv")) {
+  opl <- fread(file = csv, select = c("Name", "Sex", "Age", "BodyweightKg",
+                                      "Date", "Event", "Equipment",
+                                      "Best3SquatKg", "Best3BenchKg", "Best3DeadliftKg", "TotalKg",
+                                      "Tested", "ParentFederation")) %>%
+    rename_with(tolower) %>%
+    rename(bw = bodyweightkg,
+           squat = best3squatkg,
+           bench = best3benchkg,
+           deadlift = best3deadliftkg,
+           total = totalkg,
+           fed = parentfederation)
+  opl
 }
