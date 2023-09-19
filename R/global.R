690618f79913b0b80118dbdd24a346b176cc75bb
Petteri Mäntymaa <petteri.mantymaa@gmail.com>
2023-09-19 21:09:14 +0300

    dev

--- R/global.R
+++ R/global.R
@@ -3,16 +3,20 @@ library(data.table)
 library(tidyr)
 library(ggplot2)
 library(brms)
+library(lme4)
 
 # Disclaimer: This page uses data from the OpenPowerlifting project, https://www.openpowerlifting.org.
 # You may download a copy of the data at https://data.openpowerlifting.org.
 
 download_opl <- function() {
   url <- "https://openpowerlifting.gitlab.io/opl-csv/files/openpowerlifting-latest.zip"
-  download.file(url, here::here("data/"))
+  if(!dir.exists(here::here("data"))) dir.create(here::here("data"))
+  download.file(url, here::here("data"))
+  unzip(here::here("data/openpowerlifting-latest.zip"))
+  unlink(here::here("data/openpowerlifting-latest.zip"))
 }
 
-read_opl <- function(csv = here::here("data/openpowerlifting-2023-09-09/openpowerlifting-2023-09-09-6e6c522a.csv")) {
+read_opl <- function(csv = here::here("data/openpowerlifting-latest/openpowerlifting-2023-09-16/openpowerlifting-2023-09-16-a8e1bcb4.csv")) {
   opl <- fread(file = csv, select = c("Name", "Sex", "Age", "BodyweightKg",
                                       "Date", "Event", "Equipment",
                                       "Best3SquatKg", "Best3BenchKg", "Best3DeadliftKg", "TotalKg",
