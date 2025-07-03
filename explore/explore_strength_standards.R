2f0eb0347efd85f793bafca285aeb5403bdd52df
Petteri Mäntymaa <petteri.mantymaa@thl.fi>
2025-07-03 11:22:08 +0300

    memos

---
+++ explore/explore_strength_standards.R
@@ -0,0 +1,29 @@
+# Gangfit PHYSICAL STANDARDS
+# 
+# Strength, Speed, Aerobic Fitness & Body Fat.
+# 
+# Deadlift: 1-rep max of 2 x bodyweigth (general strength) = 180 kg @ 90 kg bodyweight
+# Chin-ups: 20 (usable upperbody strength)
+# 100m sprint: Under 13 secs (speed)
+# One mile: Under 6 minutes (endurance)
+# Body-fat: 10-12% (health)
+
+# Mitch Hooper's standards 1RM
+# Squat 285 lbs (130 kg)
+# Bench 217 lbs (100 kg)
+# Deadlift 335 lbs (150 kg)
+
+# Starting strength
+
+# Omat
+#
+# Voima
+# Kyykky 3 x 5 x 1.5 x kehonpaino = 3 x 5 x 135 kg
+# Penkki 3 x 5 x kehonpaino = 3 x 5 x 90 kg
+# Pystypunnerus 3 x 5 x 0.70 x kehonpaino =~ 3 x 5 x 62,5 kg
+# Maastaveto 1 x 5 x 2 x kehonpaino = 1 x 5 x 180 kg
+#
+# Nopeus & kestävyys
+# Maili (reilusti) alle 7 min
+# LKT-tavoitteet
+#
