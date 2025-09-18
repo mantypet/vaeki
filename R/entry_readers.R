a7963603759de11131677bc78d8d22652fa80e8f
Petteri Mäntymaa <petteri.mantymaa@thl.fi>
2025-09-18 14:37:47 +0300

    init entry data reader script

---
+++ R/entry_readers.R
@@ -0,0 +1,5 @@
+# Sets up R entry data readers
+# Created by Petteri Mäntymaa 09/2025
+
+source(here::here("R/global.R"))
+
--- R/global.R
+++ R/global.R
@@ -1,3 +1,20 @@
-library(dplyr)
-library(tidyr)
-library(ggplot2)
+# Sets up R environment
+# Created by Petteri Mäntymaa 09/2025
+
+# SET LIBRARIES
+suppressPackageStartupMessages({
+  library(dplyr)
+  library(tidyr)
+  library(stringr)
+  library(lubridate)
+  library(purrr)
+  library(glue)
+  library(rvest)
+})
+
+# Force locale
+locale_fi <- ifelse(.Platform$OS.type == "windows", "Finnish_Finland.1252", "fi_FI.UTF-8")
+foo <- Sys.setlocale(category="LC_COLLATE",locale=locale_fi)
+
+#Force working timezone
+Sys.setenv("TZ"="Europe/Helsinki")
