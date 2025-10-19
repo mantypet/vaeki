a1f89752675cf3bbfbec0a70ed741ec2ca19ec89
Petteri Mäntymaa <petteri.mantymaa@gmail.com>
2025-10-19 20:06:50 +0100

    ad hoc tests

--- explore/explore_finnish_stones.R
+++ explore/explore_finnish_stones.R
@@ -13,3 +13,17 @@ d_maakunta2025 <- wfs_read_feature(feature_name = "tilastointialueet:maakunta450
 
 finstones.rep <- finstones |>
   st_join(d_maakunta2025, join = st_within)
+
+# Read common_data/2025-10-19-finnish-stones.kml as xml
+library(xml2)
+
+doc <- read_xml(here::here("common_data/2025-10-19-finnish-stones.kml"))
+
+descriptions <- xml_find_all(doc, ".//gx:CascadingStyle")
+
+test <- xml_child(xml_child(xml_child(xml_child(xml_child(doc, 1), 2), 1), 5), 1)
+
+desc_texts <- xml_text(descriptions)
+
+length(desc_texts)
+cat(substr(desc_texts[1], 1, 300))
