3037289675e4b69577d0f6241701a448b68fec1a
Petteri Mäntymaa <petteri.mantymaa@thl.fi>
2025-10-23 11:21:50 +0100

    parse bibtex

--- explore/explore_finnish_stones.R
+++ explore/explore_finnish_stones.R
@@ -10,24 +10,50 @@ finstones.excel <- finstones |>
 
 finstones_append <- read.csv(here::here("common_data/2025-10-21-finnish-stones.csv")) |>
   as.data.frame()
- 
-feature_name <- "tilastointialueet:maakunta4500k_2025"
-crs <- 3067
-d_maakunta2025 <- wfs_read_feature(feature_name) |>
-  st_transform(crs = crs) |>
-  transmute(feature_name = feature_name,
-            maakunta_code = maakunta,
-            maakunta_name_fi = nimi,
-            maakunta_name_sv = namn,
-            maakunta_name_en = name)
 
-write_sf(d_maakunta2025,
-         dsn = here::here("common_data/2025-10-21-d_maakunta.geoJSON"),
-         driver = "GeoJSON",
-         delete_dsn = TRUE)
+# 
+# feature_name <- "tilastointialueet:maakunta4500k_2025"
+# crs <- 3067
+# d_maakunta2025 <- wfs_read_feature(feature_name) |>
+#   st_transform(crs = crs) |>
+#   transmute(feature_name = feature_name,
+#             maakunta_code = maakunta,
+#             maakunta_name_fi = nimi,
+#             maakunta_name_sv = namn,
+#             maakunta_name_en = name)
+# 
+# write_sf(d_maakunta2025,
+#          dsn = here::here("common_data/2025-10-21-d_maakunta.geoJSON"),
+#          driver = "GeoJSON",
+#          delete_dsn = TRUE)
 
+d_maakunta2025 <- read_sf(here::here("common_data/2025-10-21-d_maakunta.geoJSON"))
 
 
 finstones.rep <- finstones |>
   st_join(d_maakunta2025, join = st_within)
+
+finstone.sources <- read.xlsx(file = here::here("common_data/2025-10-21-finnish-stones-sources.xlsx"),
+                              sheetName = "sources",
+                              colClasses = "text") |>
+  filter(!is.na(bibtex))
+
+parse_bibtex <- function(bib_string) {
+  bib_string |>
+    bib2df:::bib2df_gather() |>
+    bib2df:::bib2df_tidy(separate_names = FALSE)
+}
+
+finstones.bib <- finstone.sources$bibtex |>
+  purrr::map(.f = ~parse_bibtex(.x)) |>
+  bind_rows()
+
+bib_test <- bib2df:::bib2df_gather(bib_test)
+bib_test <- bib2df:::bib2df_tidy(bib_test, separate_names = FALSE)
+
+bib2df:::bib2df
+path <- system.file("extdata", "LiteratureOnCommonKnowledgeInGameTheory.bib", package = "bib2df")
+
+df <- bib2df(path)
+debugonce(bib2df)
 
